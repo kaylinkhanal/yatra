e38e1508823593a244a9b2bc36b02b287dd2c5cd
subham <freelancerrsubham@gmail.com>
2023-08-01 20:47:21 +0545

    profile page with Avatar

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -1,87 +1,139 @@
 import React, {useState} from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import { useSelector } from 'react-redux'
-import { Card, Modal } from 'antd';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-
+import { Col, Row, Avatar, Card, Image,  Button, Space, Modal} from 'antd'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 const SignupSchema = Yup.object().shape({
-  currentPassword: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  newPassword: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-    confirmNewPassword: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required')
-});
-
- const ChangePassForm = () => {
-  return (
-    <div>
-      <Formik
-        initialValues={{
-          currentPassword: '',
-          newPassword: '',
-          confirmNewPassword: '',
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={values => {
-          // same shape as initial values
-          console.log(values);
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            <Field name="currentPassword" />
-            {errors.currentPassword && touched.currentPassword ? (
-              <div>{errors.currentPassword}</div>
-            ) : null}
-            <Field name="newPassword" />
-            {errors.newPassword && touched.newPassword ? (
-              <div>{errors.newPassword}</div>
-            ) : null}
-            <Field name="confirmNewPassword" type="confirmNewPassword" />
-            {errors.confirmNewPassword && touched.confirmNewPassword ? <div>{errors.confirmNewPassword}</div> : null}
-            <button type="submit">Submit</button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  )
- }
-const Profile = () => {
+    currentPassword: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    newPassword: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+      confirmNewPassword: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required')
+  });
+  
+  const ChangePassForm = () => {
+    return (
+      <div>
+        <Formik
+          initialValues={{
+            currentPassword: '',
+            newPassword: '',
+            confirmNewPassword: '',
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Field name="currentPassword" type="password" placeholder="Current password"/>
+              {errors.currentPassword && touched.currentPassword ? (
+                <div>{errors.currentPassword}</div>
+              ) : null}
+              <Field name="newPassword" type="password" placeholder="New password"/>
+              {errors.newPassword && touched.newPassword ? (
+                <div>{errors.newPassword}</div>
+              ) : null}
+              <Field name="confirmNewPassword" type="password" placeholder="New password" />
+              {errors.confirmNewPassword && touched.confirmNewPassword ? <div>{errors.confirmNewPassword}</div> : null}
+              <button type="submit">Change password</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    )
+   }
+export default function Profile() {
     const {userDetails} = useSelector(state=>state.users)
     const [isModalOpen, setIsModalOpen] = useState(false);
     const handleSubmit= ()=> {
         alert("submit to backend")
     }
-    return(
-  <Card title="Your Profile">
-    <Card type="inner" title="User Details" extra={<a href="#">Edit</a>}>
-      Full Name: {userDetails.fullName} <br/>
-      Email: {userDetails.email} <br/>
-      Phone: {userDetails.phoneNumber} <br/>
-      mode: {userDetails.mode}<br/>
-    </Card>
-    <Card
-      style={{
-        marginTop: 16,
-      }}
-      title="Security"
-      extra={<a href="#">More</a>}
-    >
-     <span onClick={()=>setIsModalOpen(true)}>Change Password </span><br/>
-     Delete Account
-    </Card>
-    <Modal
-    footer={null}
-    title="Change Password" open={isModalOpen} onOk={handleSubmit} onCancel={()=>setIsModalOpen(false)} >
-      <ChangePassForm/>
-      </Modal>
-  </Card>
-)};
-export default Profile;
+  return (
+    <>
+        <Header/>
+            <section className='user--profile'>
+                <div className='container'>
+                    <h2>Your Profile</h2>
+                    <Row>
+                        <Col span={12}>
+                            <Card
+                                title="Avatar"
+                                bordered={false}
+                                style={{
+                                width: 300,
+                                }}
+                            >
+                                <Image
+                                    width={'100%'}
+                                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                                />
+                                <Space
+                                    direction="vertical"
+                                    style={{
+                                    width: '100%',
+                                    marginTop:'20px'
+                                    }}
+                                >
+                                    <Button type="dashed" block>
+                                    Change Avatar
+                                    </Button>
+                                </Space>
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                        <Card className="account--details"
+                            title="Account Details"
+                            bordered={true}
+                            style={{
+                            width: '100%',
+                            }}
+                        >
+                            <p><span>Full Name: </span>{userDetails.fullName}</p>
+                            <p><span>Email: </span><a href={`mailto:${userDetails.email}`}>{userDetails.email}</a></p>
+                            <p><span>Phone: </span>{userDetails.phoneNumber}</p>
+                        </Card>
+                        <Card className="account--details"
+                            title="Account Settings"
+                            bordered={true}
+                            style={{
+                            width: '100%',
+                            }}
+                        >
+                           <Space
+                                direction="vertical"
+                                style={{
+                                width: '50%',
+                                marginTop:'20px'
+                                }}
+                            >
+                                <Button type="primary" onClick={()=>setIsModalOpen(true)}>
+                                Change Password
+                                </Button>
+                                <Modal
+                                    footer={null}
+                                    title="Change Password" open={isModalOpen} onOk={handleSubmit} onCancel={()=>setIsModalOpen(false)} >
+                                    <ChangePassForm/>
+                                </Modal>
+                            </Space>
+                        </Card>
+                        </Col>
+                    </Row>
+                </div>
+            </section>
+            
+        <Footer/>
+    </>
+  )
+}
